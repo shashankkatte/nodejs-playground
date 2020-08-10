ce09b8e8dd0a88b1e5a1fe1bec6cc8fc63a56b48
shashankkatte <shashankanand.katte@gmail.com>
2020-08-10 11:43:12 -0400

    Simple demo using file system in node

--- first-app.js
+++ first-app.js
@@ -1,1 +1,6 @@
-console.log("Hello from Node.js");
+// The obligatory hellow world print to console
+// console.log("Hello from Node.js");
+
+//A simple way to demo nodejs writing to file system
+const fs  = require('fs');
+fs.writeFileSync('hello.txt','Hello from Node.js');
