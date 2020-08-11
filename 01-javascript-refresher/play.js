8c85998d1a2ae8bf7743419b04488a015df7a4d3
shashankkatte <shashankanand.katte@gmail.com>
2020-08-11 15:59:31 -0400

    Tutorial : Template literals

--- 01-javascript-refresher/play.js
+++ 01-javascript-refresher/play.js
@@ -22,16 +22,16 @@ const hasHobbies = true;
 // }
 
 // Arrow functions : Above function can be rewritten using arrow function this way:
-const summerizeUser = (name, age, userHobbies) =>  {
+const summerizeUser = (name, age, userHobbies) => {
   return (
-      'Name is '+
-      name +
-      ', age is '+
-      age+
-      ' and the user has hobbies: '+
-      userHobbies
+    'Name is ' +
+    name +
+    ', age is ' +
+    age +
+    ' and the user has hobbies: ' +
+    userHobbies
   );
-}
+};
 
 // Short cut arrow function
 // const add = (first, second)=> first + second;
@@ -41,5 +41,17 @@ const addRandom = () => 2 + 1;
 // console.log(add(1,2));
 // console.log(addone(1));
 console.log(addRandom());
-console.log(summerizeUser(name,age,hasHobbies));
+console.log(summerizeUser(name, age, hasHobbies));
 
+// Template literals
+// Instead of using double or single quotation marks:
+// 'A String'
+// or
+// "Another string"
+// you can use backticks (`)
+// `Another way of writing strings`
+// Now why would we use that way of creating strings?
+// With that syntax, you can dynamically add data into a string like this:
+const nameAgain = 'yourself';
+const ageAgain = 29;
+console.log(`My name is ${nameAgain} and I am ${ageAgain} years old.`);
