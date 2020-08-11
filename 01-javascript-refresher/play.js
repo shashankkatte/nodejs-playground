3a5d4c7eaf4316cac22f55ac08d2aa9344505e9f
shashankkatte <shashankanand.katte@gmail.com>
2020-08-11 10:20:24 -0400

    Doc : Update comments for let and cosnt usage

--- 01-javascript-refresher/play.js
+++ 01-javascript-refresher/play.js
@@ -4,6 +4,7 @@
 // var hasHobbies = true;
 
 // Var is outdated way of declaring variables use let and const instead to make the declaration more clear and intentional
+// As a rule of thumb: use let instead of var, and for the variables that dont change in value use const
 const name = 'Tony Stark';
 let age = 35;
 const hasHobbies = true;
