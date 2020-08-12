39952a53bdecbeb789d6bb5b80a700436bac7569
shashankkatte <shashankanand.katte@gmail.com>
2020-08-12 11:58:02 -0400

    app.js : Parsing url and sending conditional response back

--- 02-node-server/app.js
+++ 02-node-server/app.js
@@ -5,7 +5,17 @@ const http = require('http');
 
 // the http object has the create server method which does what its named after - creates a server. it takes a call back function as an argument here we use an anonymous function and are logging request object to the console
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  //   parse the url
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Response from node</title></head>');
