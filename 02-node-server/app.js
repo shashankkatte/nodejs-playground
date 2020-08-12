a68a0439064044d68a2a8dd17c5baec7e014496f
shashankkatte <shashankanand.katte@gmail.com>
2020-08-12 11:34:35 -0400

    app.js : Sending a simple html response

--- 02-node-server/app.js
+++ 02-node-server/app.js
@@ -6,6 +6,12 @@ const http = require('http');
 // the http object has the create server method which does what its named after - creates a server. it takes a call back function as an argument here we use an anonymous function and are logging request object to the console
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Response from node</title></head>');
+  res.write('<body><h1>Hello from Node.js Server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 // Now we need this server to be always listening so... this is how we do it.
