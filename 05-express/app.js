c64f3e00f2796037a4853de17e659719cebef4c9
shashankkatte <shashankanand.katte@gmail.com>
2020-08-14 12:39:55 -0400

    Bringing up server with express.js

--- 05-express/app.js
+++ 05-express/app.js
@@ -4,6 +4,18 @@ const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // This allows the request to continue to the next middleware
+});
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+   res.send('<h1> Hello from Express</h1>');
+});
 
-server.listen(3000);
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+//Using express.js above two lines of code can be condensed into just
+app.listen(3000);
