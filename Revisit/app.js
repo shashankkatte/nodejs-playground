9a17a721fadca6704fadf6dbc7e119f3273edba1
shashankkatte <shashankanand.katte@gmail.com>
2020-09-14 14:27:28 -0400

    Simple server in 3 lines

--- Revisit/app.js
+++ Revisit/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+const server = http.createServer((req,res)=>{
+    console.log(req);
 });
 
 server.listen(3000);
